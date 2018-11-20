6b5c7afb250193b82e30ed62a2c22b4573e5f9d1
wonder.199806@gmail.com <457644009@qq.com>
2018-11-20 16:33:16 +0800

    opencvJS using jQuery

--- script/opencv_script.js
+++ script/opencv_script.js
@@ -1,5 +1,5 @@
 function onOpenCvReady() {
-    $('#black').css('display', 'none');
+    $('#black').fadeOut(300);
 }
 
 // 获取某通道的直方图数据
@@ -7,26 +7,25 @@ const getHistogramData = function (img, channel) {          // 0->R, 1->G, 2->B,
     let histogramData = new Array(256);
     for (let i = 0; i < 256; i++) histogramData[i] = 0;         // initialize
     // 逐行扫描
-    for (let i = 0; i < img.rows; i++) {
+    for (let i = 0; i < img.rows; i++)
         for (let j = 0; j < img.cols; j++) {
             let pixelData = img.ucharPtr(i, j);
             let valueData = pixelData[channel];
             histogramData[valueData]++;
         }
-    }
     return histogramData;
 }
 
-const srcImg = document.getElementById('srcImg');           // 原图的img标签
-document.getElementById('srcImgInputButton').onchange = function(e) {
+const srcImg = $('#srcImg')[0];           // 原图的img标签
+$('#srcImgInputButton').change(function(e) {
     srcImg.src = URL.createObjectURL(e.target.files[0]);
-}
+});
 srcImg.onload = function () {
     let srcMat = cv.imread(srcImg);
-    document.getElementById('img_size').innerHTML = srcMat.size().width + '×' + srcMat.size().height;
-    document.getElementById('img_channels').innerHTML = srcMat.channels();
-    document.getElementById('img_depth').innerHTML = srcMat.depth();
-    document.getElementById('img_type').innerHTML = srcMat.type();
+    $('#img_size').html(srcMat.size().width + '×' + srcMat.size().height);
+    $('#img_channels').html(srcMat.channels());
+    $('#img_depth').html(srcMat.depth());
+    $('#img_type').html(srcMat.type());
 
     cv.imshow('currentImgCanvas', srcMat);
     srcMat.delete();
