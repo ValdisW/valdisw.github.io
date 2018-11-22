447df461e5a46f692e5d0df1b8e32524ed1ce9e6
wonder.199806@gmail.com <457644009@qq.com>
2018-11-22 19:33:41 +0800

    add linearGrayTrans to opencvJS

--- script/opencv_script.js
+++ script/opencv_script.js
@@ -1,3 +1,5 @@
+let currentMat = void 0;
+
 function onOpenCvReady() {
     $('#black').fadeOut(300);
 }
@@ -16,17 +18,71 @@ const getHistogramData = function (img, channel) {          // 0->R, 1->G, 2->B,
     return histogramData;
 }
 
+// 线性灰度变换
+const linearGrayTrans = function (img, f1, f2, t1, t2) {
+    let k = (t2 - t1) / (f2 - f1),
+        b = (f1 * t2 - f2 * t1) / (f1 - f2);
+    for (let i = 0; i < img.rows; i++) {
+        for (let j = 0; j < img.cols; j++) {
+            let pixelData = img.ucharPtr(i, j);
+            for (let c = 0; c < 3; c++) {
+                if (pixelData[c] >= f1 && pixelData[c] <= f2) {
+                    let a = Math.round(k * pixelData[c]);
+                    pixelData[c] = a + b;
+                }
+            }
+        }
+    }
+    cv.imshow('currentImgCanvas', currentMat);      // 显示
+}
+
+// 读取图片
 const srcImg = $('#srcImg')[0];           // 原图的img标签
 $('#srcImgInputButton').change(function(e) {
     srcImg.src = URL.createObjectURL(e.target.files[0]);
 });
 srcImg.onload = function () {
     let srcMat = cv.imread(srcImg);
-    $('#img_size').html(srcMat.size().width + '×' + srcMat.size().height);
-    $('#img_channels').html(srcMat.channels());
-    $('#img_depth').html(srcMat.depth());
-    $('#img_type').html(srcMat.type());
+    currentMat = srcMat;
+    $('#img_size').html(currentMat.size().width + '×' + currentMat.size().height);
+    $('#img_channels').html(currentMat.channels());
+    $('#img_depth').html(currentMat.depth());
+    $('#img_type').html(currentMat.type());
 
-    cv.imshow('currentImgCanvas', srcMat);
-    srcMat.delete();
-}
+    // 添加canvas元素用于显示打开的图像，并附加于div窗口上
+    let imageCanvas = $('<canvas id="currentImgCanvas"></canvas>');
+    $('#mainFrame').append(imageCanvas);
+    let currentImgCanvas = $('#currentImgCanvas');
+    let mouseDown = false,              // 判断拖动
+        mouseX = void 0,                    // 按下时鼠标坐标
+        mouseY = void 0,
+        preTop = void 0,                    // 按下时窗口的top
+        preLeft = void 0;
+    currentImgCanvas.css({
+        'display': 'inline-block',
+        'cursor': 'pointer',
+        'position': 'fixed',
+        'top': '80px',
+        'left': '80px',
+    }).mousedown(function (e) {             // 鼠标按下
+        e = e?e:window.event;
+        mouseDown = true;
+        mouseX = e.clientX; mouseY = e.clientY;
+        preTop = parseInt(this.style.top); preLeft = parseInt(this.style.left);
+    }).mousemove(function (e) {             // 鼠标拖动（移动
+        e = e?e:window.event;
+        if (mouseDown) {
+            currentImgCanvas.css('top', preTop + e.clientY - mouseY+'px');
+            currentImgCanvas.css('left', preLeft + e.clientX - mouseX+'px');
+        }
+    }).mouseup(function () {                 // 鼠标松开
+        mouseDown = false;
+    });
+    cv.imshow('currentImgCanvas', srcMat);      // 显示
+    //srcMat.delete();
+};
+
+$('#linearGrayTransButton').click(function () {
+    linearGrayTrans(currentMat, 0, 128, 0, 32);
+    console.log('23333333');
+});
