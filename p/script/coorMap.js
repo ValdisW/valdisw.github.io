ba561f4b3028a86e27debd9c50a6be5525a4aee1
Valdis Wong <457644009@qq.com>
2019-01-27 22:35:33 +0800

    resolution fixed

--- p/script/coorMap.js
+++ p/script/coorMap.js
@@ -28,7 +28,7 @@ for (let i = 0; i < blockVertexData.length; i++) {
 
 // 调整各管线以适应屏幕
 for (let node in pipeVertexData) {
-    pipeVertexData[node].coor[0] = (pipeVertexData[node].coor[0] - minX) * window.innerWidth / (maxX - minX) / 2 + 400;
+    pipeVertexData[node].coor[0] = (pipeVertexData[node].coor[0] - minX) * window.innerWidth / (maxX - minX) / 2 + window.innerWidth / 4;
     pipeVertexData[node].coor[1] = (pipeVertexData[node].coor[1] - minY) * (window.innerHeight - title_height) / (maxY - minY) *0.98 + title_height;
 }
 let pipeVertexData_origin = JSON.parse(JSON.stringify(pipeVertexData));     // 保留原始坐标的拷贝
@@ -38,7 +38,7 @@ for (let i = 0; i < pipes_noData_drawData.length; i++) {
     pipes_noData_drawData[i] = new Array(pipeData_noData[i].length);
     for (let segment = 0; segment < pipeData_noData[i].length; segment++) {
         pipes_noData_drawData[i][segment] = new Array(2);
-        pipes_noData_drawData[i][segment][0] = (pipeData_noData[i][segment][0] - minX) * window.innerWidth / (maxX - minX) / 2 + 400;
+        pipes_noData_drawData[i][segment][0] = (pipeData_noData[i][segment][0] - minX) * window.innerWidth / (maxX - minX) / 2 + window.innerWidth / 4;
         pipes_noData_drawData[i][segment][1] = (pipeData_noData[i][segment][1] - minY) * (window.innerHeight - title_height) / (maxY - minY) *0.98 + title_height;
     }
 }
@@ -53,8 +53,8 @@ for (let i = 0; i < pipes_noData_drawData_origin.length; i++) {
 // 调整各水池以适应屏幕
 let blockData_fit = blockVertexData.slice(0);
 for (let i = 0; i < blockData_fit.length; i++){
-    blockData_fit[i].vertex1[0] = (blockData_fit[i].vertex1[0] - minX) * window.innerWidth / (maxX - minX) / 2 + 400;
-    blockData_fit[i].vertex2[0] = (blockData_fit[i].vertex2[0] - minX) * window.innerWidth / (maxX - minX) / 2 + 400;
+    blockData_fit[i].vertex1[0] = (blockData_fit[i].vertex1[0] - minX) * window.innerWidth / (maxX - minX) / 2 + window.innerWidth / 4;
+    blockData_fit[i].vertex2[0] = (blockData_fit[i].vertex2[0] - minX) * window.innerWidth / (maxX - minX) / 2 + window.innerWidth / 4;
     blockData_fit[i].vertex1[1] = (blockData_fit[i].vertex1[1] - minY) * (window.innerHeight - title_height) / (maxY - minY) *0.98 + title_height;
     blockData_fit[i].vertex2[1] = (blockData_fit[i].vertex2[1] - minY) * (window.innerHeight - title_height) / (maxY - minY) *0.98 + title_height;
 }
