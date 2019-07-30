ccf5a9aef50f5b41f914a0b85e1196570c143e18
ValdisW <457644009@qq.com>
2019-07-31 01:50:46 +0800

    update echarts option

--- mountain/script/mountainOption.js
+++ mountain/script/mountainOption.js
@@ -7,13 +7,14 @@ function getMountainOption(city, data) {
             data: data.map(function (item) {
                 return item[0];
             }),
-            show: false
         },
         yAxis: {
             splitLine: {
                 show: false
             },
-            show: false
+        },
+        tooltip: {
+            trigger: 'axis'
         },
         toolbox: {
             left: 'center',
@@ -25,47 +26,71 @@ function getMountainOption(city, data) {
                 saveAsImage: {}
             }
         },
+        visualMap: {
+            top: 10,
+            right: 10,
+            pieces: [{
+                gt: 0,
+                lte: 50,
+                color: '#096'
+            }, {
+                gt: 50,
+                lte: 100,
+                color: '#ffde33'
+            }, {
+                gt: 100,
+                lte: 150,
+                color: '#ff9933'
+            }, {
+                gt: 150,
+                lte: 200,
+                color: '#cc0033'
+            }, {
+                gt: 200,
+                lte: 300,
+                color: '#660099'
+            }, {
+                gt: 300,
+                color: '#7e0023'
+            }],
+            outOfRange: {
+                color: '#999'
+            }
+        },
         animation: false,
         series: {
             name: city + ' AQI',
             type: 'line',
-            smooth: false,
-            itemStyle: {
-                opacity: 0
-            },
-            lineStyle: {
-                opacity: 0
-            },
             data: data.map(function (item) {
                 return item[1];
             }),
-            // markLine: {
-            //   silent: true,
-            //   data: [{
-            //     yAxis: 50
-            //   }, {
-            //     yAxis: 100
-            //   }, {
-            //     yAxis: 150
-            //   }, {
-            //     yAxis: 200
-            //   }, {
-            //     yAxis: 300
-            //   }]
-            // },
-            areaStyle: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                    offset: 0,
-                    color: 'rgb(80, 166, 191)'
+            markLine: {
+                silent: true,
+                data: [{
+                    yAxis: 50
                 }, {
-                    offset: 0.5,
-                    color: 'rgb(156, 196, 139)'
+                    yAxis: 100
                 }, {
-                    offset: 1,
-                    color: 'rgb(179, 157, 83)'
-                }]),
-                opacity: 1
+                    yAxis: 150
+                }, {
+                    yAxis: 200
+                }, {
+                    yAxis: 300
+                }]
             },
+            // areaStyle: {
+            //     color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+            //         offset: 0,
+            //         color: 'rgb(80, 166, 191)'
+            //     }, {
+            //         offset: 0.5,
+            //         color: 'rgb(156, 196, 139)'
+            //     }, {
+            //         offset: 1,
+            //         color: 'rgb(179, 157, 83)'
+            //     }]),
+            //     opacity: 1
+            // },
         }
     }
 };
