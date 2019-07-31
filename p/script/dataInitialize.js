2fcdf95c0a6fac1c754972a32a12a91074a767fe
ValdisW <457644009@qq.com>
2019-07-31 16:45:44 +0800

    test

--- p/script/dataInitialize.js
+++ p/script/dataInitialize.js
@@ -10,7 +10,7 @@ for (let node in pipeVertexData) {
 }
 $.getJSON('./data/output.json', data => {
     for (let p of data) {
-        console.log('p.speed =', p.speed.toFixed(2));
+        console.log('p.speed =', p.speed.toFixed(4));
         let speed = parseInt(p.speed).toFixed(2);
         let pipe_id = 'g' + p.pipe;
         for (let map of node_pipe_map) {
